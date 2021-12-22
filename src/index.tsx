b9b34543ffc6ef343ed5446ea3f9045b11749c7d
Angel Paunchev <apaunchev@gmail.com>
2021-12-22 09:28:36 +0200

    Remove extra component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import ReactDOM from "react-dom";
-import { Playground } from "./components/playground";
+import { Editor } from "./components/editor";
+import { Preview } from "./components/preview";
 
-const App = () => {
-  return <Playground />;
+const initialSnippet = {
+  html: `<div id="root"></div>`,
+  css: `#root { font-family: sans-serif; }`,
+  javascript: `
+    import * as React from 'https://cdn.skypack.dev/react';
+    import * as ReactDOM from 'https://cdn.skypack.dev/react-dom';
+    const App = () => <h1>App</h1>;
+    ReactDOM.render(<App />, document.getElementById("root"));
+  `,
+};
+
+const App: React.FC = () => {
+  const [snippet, setSnippet] = useState(initialSnippet);
+
+  const handleChange = (changedContent: string, changedType: string) => {
+    setSnippet((snippet) => ({
+      ...snippet,
+      [changedType]: changedContent,
+    }));
+  };
+
+  return (
+    <div className="playground">
+      <Editor snippet={snippet} onChange={handleChange} />
+      <Preview snippet={snippet} />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
